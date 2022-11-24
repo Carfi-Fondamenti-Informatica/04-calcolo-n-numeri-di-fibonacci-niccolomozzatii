64abdb5c7dff6936af82abb1cc37436e35b3d200
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-11-24 19:41:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   int primo_numero=0;
+   int secondo_numero=0;
+   int n;
+   cin >> n;
+   int i=2;
+   if (n>=2){
+      cout << 1 << endl;
+     while (i++ <= n){
+        cout << primo_numero + secondo_numero << endl;
+        if (primo_numero < secondo_numero)
+           primo_numero += secondo_numero;
+        else secondo_numero += primo_numero;
+     }
+   }else {
+      cout << "errore" << endl;
+   }
    return 0;
 }
